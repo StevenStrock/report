53fdc7102a90ffd7ac4730cf504eecc8ff6b6b9f
eclipse_user <eclipse_email@zyscp.com>
2020-09-07 18:50:54 +0800

    update BnsController,addregister function

--- src/main/java/com/zys/platform/controller/BnsController.java
+++ src/main/java/com/zys/platform/controller/BnsController.java
@@ -1,5 +1,16 @@
 package com.zys.platform.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class BnsController {
+	@ResponseBody
+	@RequestMapping("/bns/register")
+	public String register() {
+		
+		return "success";
+	}
 
 }
