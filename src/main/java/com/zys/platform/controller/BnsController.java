b302de7fa90fff998cd12c4b4bde49724d670740
eclipse_user <eclipse_email@zyscp.com>
2020-09-07 19:19:27 +0800

    update

--- src/main/java/com/zys/platform/controller/BnsController.java
+++ src/main/java/com/zys/platform/controller/BnsController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+//@Controller
 public class BnsController {
-	@ResponseBody
-	@RequestMapping("/bns/register")
-	public String register() {
-		
-		return "success";
-	}
+//	@ResponseBody
+//	@RequestMapping("/bns/register")
+//	public String register() {
+//		
+//		return "success";
+//	}
 
 }
